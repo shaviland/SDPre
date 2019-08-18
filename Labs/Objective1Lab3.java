67c42e6938d45c7fcc55417d0372b65bd4805809
Samantha Haviland <shaviland@live.com>
2019-08-17 23:28:31 -0600

    First Batch of Labs

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main( String[] args ) {
+    System.out.println( "Name " + ":" + "Samantha Haviland" );
+    System.out.println( "Age : 31" );
+    System.out.println( "State : Texas" );
+  }
+}
